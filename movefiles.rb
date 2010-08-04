6cfd392defa310e6e6604f3c56e0421d73938b08
David Pick <david@david-laptop.(none)>
2010-08-04 16:01:57 -0500

    added move files script

---
+++ movefiles.rb
@@ -0,0 +1,49 @@
+require 'fileutils'
+
+MEDIAPATH = '/home/david/code/move_files/'
+
+
+def tvDirectory(show)
+  if show =~ /^[A-Ha-h]/
+    return 'tv'
+  elsif show =~ /^[I-Pi-p]/
+    return 'tv2'
+  else
+    return 'tv3'
+  end
+end
+
+#checks if show and season folders exists, if they don't create them
+def foldersExist?(show, season)
+  if !File.directory?(MEDIAPATH + tvDirectory(show))
+    puts 'directory did not exist, creating it'
+    Dir.mkdir(MEDIAPATH + tvDirectory(show))
+  end
+
+  if !File.directory?(MEDIAPATH + tvDirectory(show) + '/' + show)
+    puts 'directory did not exist, creating it'
+    Dir.mkdir(MEDIAPATH + tvDirectory(show) + '/' + show)
+  end
+
+  if !File.directory?(MEDIAPATH + tvDirectory(show) + '/' + show + '/Season ' + season)
+    puts 'directory did not exist, creating it'
+    Dir.mkdir(MEDIAPATH + tvDirectory(show) + '/' + show + '/Season ' + season)
+  end
+end
+
+dir = Dir.open(MEDIAPATH) 
+
+dir.each do |filename|
+  if filename =~ /([0-9]+)x([0-9]*)/ 
+    season = $1
+    episode = $2
+    show = filename.gsub(/ -.*/, '')
+    if show =~ /^The/
+      show = show.gsub(/^The /, '')
+    end
+
+    foldersExist?(show, season)
+
+    FileUtils.mv(filename, MEDIAPATH + tvDirectory(show) + '/' + show + '/Season ' + season + '/' + filename)
+  end
+end
